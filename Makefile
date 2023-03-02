5c1dec7ae9ed9af8e094991a368d0c6200806c96
emoney17 <emmanuel.million@tutanota.com>
2023-03-02 12:46:10 -0600

    load bmp

--- Makefile
+++ Makefile
@@ -4,5 +4,3 @@ LIBS = -lSDL2
 SRC = ./main.c
 all:
 	$(CC) $(CFLAGS) -o run $(SRC) $(LIBS)
-run: all 
-	./run
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 int main(void)
@@ -24,9 +25,24 @@ int main(void)
 	fprintf(stderr, "Surface Error: %s\n", SDL_GetError());
 	return -1;
     }
+    SDL_Surface* image = SDL_LoadBMP("./test.bmp");
+    if (!image) {
+	fprintf(stderr, "Image load Error: %s\n", SDL_GetError());
+	return -1;
+    }
 
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(2500);
-
+    bool running = true;
+    while (running) {
+	SDL_Event e;
+	while (SDL_PollEvent(&e) > 0) {
+	    switch(e.type) {
+	    case SDL_QUIT:
+		running = false;
+		break;
+	    }
+	    SDL_BlitSurface(image, NULL, window_surface, NULL);
+	    SDL_UpdateWindowSurface(window);
+	}
+    }
     return 0;
 }
