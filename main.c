a4fb6ae7ff131734250792a86b6c5d99ea42e9ed
emoney17 <emmanuel.million@tutanota.com>
2023-03-02 12:53:28 -0600

    fix indent?

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 int main(void)
 {
     // Initialize SDL2
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
 	fprintf(stderr, "Init Error: %s\n", SDL_GetError());
 	return -1;
     }
@@ -15,27 +16,33 @@ int main(void)
 	SDL_WINDOWPOS_CENTERED, // X pos
 	SDL_WINDOWPOS_CENTERED, // Y pos
 	680, 400, 0); // Width, Height, Success
-    if (!window) {
+    if (!window)
+    {
 	fprintf(stderr, "Window Error: %s\n", SDL_GetError());
 	return -1;
     }
 
     SDL_Surface *window_surface = SDL_GetWindowSurface(window);
-    if (!window_surface) {
+    if (!window_surface)
+    {
 	fprintf(stderr, "Surface Error: %s\n", SDL_GetError());
 	return -1;
     }
     SDL_Surface* image = SDL_LoadBMP("./test.bmp");
-    if (!image) {
+    if (!image)
+    {
 	fprintf(stderr, "Image load Error: %s\n", SDL_GetError());
 	return -1;
     }
 
     bool running = true;
-    while (running) {
+    while (running)
+    {
 	SDL_Event e;
-	while (SDL_PollEvent(&e) > 0) {
-	    switch(e.type) {
+	while (SDL_PollEvent(&e) > 0)
+	{
+	    switch(e.type)
+	    {
 	    case SDL_QUIT:
 		running = false;
 		break;
