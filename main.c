6c42a44a991b97f4055251b746f099b395dd1110
emoney17 <emmanuel.million@tutanota.com>
2023-03-03 11:19:34 -0600

    function for moving

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_stdinc.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
@@ -5,36 +7,43 @@
 #define TITLE "SDL2 Window"
 #define WIDTH 680
 #define HEIGHT 400
+#define delta_time 1.0/60.0 // Cap framerate to 60fps
+
+enum Direction{ NONE, UP, DOWN, LEFT, RIGHT };
+enum Direction direction;
+
+void process_keys(SDL_Event event);
+void process_directions(double* x, double* y);
 
 int main(void)
 {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-	fprintf(stderr, "Init Error: %s\n", SDL_GetError());
-	return -1;
+	    fprintf(stderr, "Init Error: %s\n", SDL_GetError());
+	    return -1;
     }
 
     SDL_Window *window = SDL_CreateWindow(
-	TITLE,
-	SDL_WINDOWPOS_CENTERED,
-	SDL_WINDOWPOS_CENTERED,
-	WIDTH,
-	HEIGHT,
-	0);
+	    TITLE,
+	    0,
+	    0,
+	    WIDTH,
+	    HEIGHT,
+	    0);
     if (!window) {
-	fprintf(stderr, "Window Error: %s\n", SDL_GetError());
-	return -1;
+	    fprintf(stderr, "Window Error: %s\n", SDL_GetError());
+	    return -1;
     }
 
     SDL_Surface *window_surface = SDL_GetWindowSurface(window);
     if (!window_surface) {
-	fprintf(stderr, "Surface Error: %s\n", SDL_GetError());
-	return -1;
+	    fprintf(stderr, "Surface Error: %s\n", SDL_GetError());
+	    return -1;
     }
 
     SDL_Surface* image = SDL_LoadBMP("./Untitled.bmp");
     if (!image) {
-	fprintf(stderr, "Image load Error: %s\n", SDL_GetError());
-	return -1;
+	    fprintf(stderr, "Image load Error: %s\n", SDL_GetError());
+	    return -1;
     }
 
     SDL_Rect image_position;
@@ -43,28 +52,77 @@ int main(void)
     image_position.w = 50;
     image_position.h = 50;
 
-    double delta_time = 1.0/60.0; // Cap framerate to 60fps
+    double x = 0.0;
+    double y = 0.0;
     bool running = true; // App is running
-    double image_x = 0; // For double to int convers in Rect
-    double image_y = 0; // For double to int convers in Rect
-    SDL_Event e;
 
+    SDL_Event e;
     while (running) {
-	while (SDL_PollEvent(&e) > 0) {
-	    switch(e.type) {
-	    case SDL_QUIT:
-		running = false;
-		break;
+	    while (SDL_PollEvent(&e) > 0) {
+            process_keys(e);
+	        switch(e.type) {
+	            case SDL_QUIT:
+		        running = false;
+		        break;
+	        }
 	    }
-	}
-	image_x = image_x + (5 * delta_time); // Move image every frame
-	image_position.x = image_x;
-	SDL_FillRect(window_surface, NULL,
-		     SDL_MapRGB(window_surface->format, 0, 0, 0)); // Clean the screen before draw 
-	SDL_BlitSurface(image, NULL, window_surface, &image_position);
-	SDL_UpdateWindowSurface(window);
+	    process_directions(&x, &y);
+	    image_position.x = x;
+	    image_position.y = y;
+	    SDL_FillRect(window_surface, NULL,
+		     SDL_MapRGB(window_surface->format, 0, 0, 0)); // Clean the screen before draw
+	    SDL_BlitSurface(image, NULL, window_surface, &image_position);
+	    SDL_UpdateWindowSurface(window);
     }
     SDL_FreeSurface(window_surface);
     SDL_DestroyWindow(window);
     return 0;
+}
+
+void process_keys(SDL_Event event)
+{
+    Uint8 const* keys;
+    switch(event.type) {
+        case SDL_KEYDOWN:
+            keys = SDL_GetKeyboardState(NULL);
+            if (keys[SDL_SCANCODE_W] == 1) {
+                direction = UP;
+                printf("UP\n");
+            }
+            else if (keys[SDL_SCANCODE_S] == 1) {
+                direction = DOWN;
+                printf("DOWN\n");
+            }
+            else if (keys[SDL_SCANCODE_A] == 1) {
+                direction = LEFT;
+                printf("LEFT\n");
+            }
+            else if (keys[SDL_SCANCODE_D] == 1) {
+                printf("RIGHT\n");
+                direction = RIGHT;
+            }
+            break;
+    }
+}
+
+void process_directions(double* x, double* y)
+{
+    switch(direction) {
+        case NONE:
+	    *x += 0.0;
+	    *y += 0.0;
+	    break;
+        case UP:
+	    *y  = *y - (5.0 * delta_time);
+            break;
+        case DOWN:
+            *y = *y + (5.0 * delta_time);
+            break;
+        case LEFT:
+            *x = *x - (5.0 * delta_time);
+            break;
+        case RIGHT:
+            *x = *x + (5.0 * delta_time);
+            break;
+    }
 }
