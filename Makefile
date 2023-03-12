717b59d3bc316d729f3fa6ea8cd17c4fdbb4b774
emoney17 <emmanuel.million@tutanota.com>
2023-03-11 18:20:17 -0600

    commit

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -std=c11 -pedantic -ggdb
 LIBS = -lSDL2 -lSDL2_image
-SRC = ./main.c
+SRC = ./main.c ./window.c
 all:
 	$(CC) $(CFLAGS) $(SRC) $(LIBS)
--- main.c
+++ main.c
@@ -1,87 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <math.h>
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-
-#define FPS 60
-#define FRAME_DELAY 1000/60
-#define W_TITLE "cube"
-#define W_WIDTH 500
-#define W_HEIGHT 500
-
-void init(SDL_Window** window, SDL_Renderer** renderer);
+#include "./window.h"
 
 int main(void)
 {
+    // Setup
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
-    init(&window, &renderer);
-
+    create_window_renderer(&window, &renderer);
     bool running = true;
     SDL_Event e;
 
-    unsigned int frameStart;
-    int frameTime;
-    int count = 0;
-
-    while (running) {
-	frameStart = SDL_GetTicks(); 
-
-	while (SDL_PollEvent(&e)) {
-	    if (e.type == SDL_QUIT)
+    // Window loop
+    while (running)
+    {
+	clear(renderer); // Clear window to black
+	while(SDL_PollEvent(&e))
+	{
+	    switch(e.type)
+	    {
+	    case SDL_QUIT:
 		running = false;
+	    }
 	}
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 225);
-	SDL_RenderClear(renderer);
-
-	SDL_SetRenderDrawColor(renderer, 225, 225, 225, 225);
-	SDL_RenderDrawLine(renderer, 100, 100, 200, 100);
-	SDL_RenderDrawLine(renderer, 200, 100, 200, 200);
-	SDL_RenderDrawLine(renderer, 200, 200, 100, 200);
-	SDL_RenderDrawLine(renderer, 100, 200, 100, 100);
-
-	SDL_RenderDrawLine(renderer, 150, 150, 250, 150);
-	SDL_RenderDrawLine(renderer, 250, 150, 250, 250);
-	SDL_RenderDrawLine(renderer, 250, 250, 150, 250);
-	SDL_RenderDrawLine(renderer, 150, 250, 150, 150);
-
-
-	SDL_RenderDrawLine(renderer, 100, 100, 150, 150);
-	SDL_RenderDrawLine(renderer, 200, 100, 250, 150);
-	SDL_RenderDrawLine(renderer, 200, 200, 250, 250);
-	SDL_RenderDrawLine(renderer, 100, 200, 150, 250);
-
-	frameTime = SDL_GetTicks() - frameStart; 
-	if (FRAME_DELAY >= frameTime) 
-	    SDL_Delay(FRAME_DELAY - frameTime);
-
-	printf("%d\n", count);
-	count++;
-	SDL_RenderPresent(renderer);
+	draw(renderer); // Draw things from renderer
     }
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    IMG_Quit();
-    SDL_Quit();
-
+    // Cleanup
+    clean(window, renderer);
     return 0;
-}
-
-void init(SDL_Window** window, SDL_Renderer** renderer)
-{
-    if (SDL_Init(SDL_INIT_VIDEO) > 0) fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
-    if (!(IMG_Init(IMG_INIT_PNG))) fprintf(stderr, "Error initializing SDL image: %s\n", SDL_GetError());
-
-    *window = SDL_CreateWindow(W_TITLE, 0, 0, W_WIDTH, W_HEIGHT, 0);
-    if (!*window) fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
-
-    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
-    if (!*renderer) fprintf(stderr, "Error creating renderer: %s\n", SDL_GetError());
-}
-
-void drawCube(int cube[])
-{
-    
 }
